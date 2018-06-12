905ea047bc05349398fc711ae69f834073b6a6bb
laytonj <jordanlayton@gmail.com>
2018-06-12 12:57:24 -0400

    d3 test

--- js/d3test.js
+++ js/d3test.js
@@ -1,16 +1,19 @@
-var svg = d3.select('#viz').append('svg')
+var svgFromJson = d3.select('#vizFromJson').append('svg')
   .attr('height', 500)
   .attr('width', 500)
   .style('background-color', '#f3f3f3');
 
-  
+var svgFromCsv = d3.select('#vizFromCsv').append('svg')
+  .attr('height', 500)
+  .attr('width', 500)
+  .style('background-color', '#f4f4f4');
   
 d3.json('../../../yml_to_json/d3test.json', function(error, data) {
   if (error) throw error;
 
   console.log(data);
   
-  svg.selectAll('rect')
+  svgFromJson.selectAll('rect')
     .data(data)
     .enter()
     .append('rect')
@@ -23,4 +26,15 @@ d3.csv('../../../yml_to_csv/d3test.csv', function(error, data) {
   if (error) throw error;
 
   console.log(data);
+
+  svgFromCsv.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('height', function(d){return d.h * 1.5;})
+    .attr('width', function(d){return d.w * 1.7;})
+    .attr('transform', function(d,i){return 'translate(' + (i * 60) + ',' + (i * 70) + ')'})
+    .style('cursor','pointer')
+    .on('click', function(){d3.select(this).style('fill','blue')});
+  
 });
